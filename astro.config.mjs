7e09d0c1c025a0414519f816e9ab797b56e56991
MendozaNicolas <nicolas.mendozaion@gmail.com>
2024-06-24 23:44:18 -0300

    refactor to ceibo brand

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://mendozanicolas.github.io',
-  base: 'aggiornare-soluciones',
+  site: 'https://estudioceibo.com.ar',
 });
